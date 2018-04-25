989cfeccb506595e5e33c6c5dfcf787ac6a7203f
yanchu1990@hotmail.com <yanchu1990@hotmail.com>
2018-04-25 17:53:26 +0800

    20180425

--- oracle函数示例.sql
+++ oracle函数示例.sql
@@ -18,8 +18,30 @@ end sp_formatdate;
 create?or?replace?function?get_user???
 return?varchar2???
 is???
-Result?varchar2(50);?--定义变量??
+Result?varchar2(50);--定义变量??
 begin???
 select?username?into?Result?from?user_users;???
-return(Result);?--返回值??
-end?get_user;??
+return(Result);--返回值??
+end?get_user;
+
+------------------------------------
+CREATE OR REPLACE FUNCTION testFunc (num1 IN NUMBER, num2 IN NUMBER)
+RETURN NUMBER
+AS
+   num3 number;
+   num4 number;
+   num5 number;
+BEGIN
+   num3 := num1 + num2;
+   num4 := num1 * num2;
+   num5 := num3 * num4; 
+   RETURN num5;
+END;  
+
+begin
+num number(38,0);
+num := testfunc(1,2));
+dbms_output.put_line(num);
+end;
+
+ 
